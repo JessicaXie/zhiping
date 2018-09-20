b7ce609ba35a0795120de5242b74b62ba3b7a179
JessicaXie <15210277136@163.com>
2018-09-20 08:25:09 +0800

    merge

--- src/componnets/nav-footer/nav-footer.jsx
+++ src/componnets/nav-footer/nav-footer.jsx
@@ -3,10 +3,19 @@ import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
 import {TabBar} from 'antd-mobile'
 
+const Item = TabBar.Item
+
 class NavFooter extends Component{
+  static propTypes = 
+
   render(){
     return(
-      <div></div>
+      <TabBar>
+        {
+
+        }
+
+      </TabBar>
     )
   }
 }
